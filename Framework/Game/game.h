5cb98ad146b1152a800d63a28a94886b33b02d96
xhz8s <52607377+xhz8s@users.noreply.github.com>
2019-09-03 17:31:51 +0200

    Update game.h

--- Framework/Game/game.h
+++ Framework/Game/game.h
@@ -14,198 +14,271 @@ struct vector2
 
 namespace ph
 {
-	namespace off
+	namespace Offsets
 	{
-		extern DWORD64 ptr_world;
-		extern DWORD64 ptr_nearest_ped;
-		extern DWORD64 ptr_waypoint_x;
-		extern DWORD64 ptr_waypoint_y;
-		extern DWORD64 ptr_crosshair;
+		extern DWORD64 ptrWorld;
+		extern DWORD64 ptrWaypointX;
+		extern DWORD64 ptrWaypointY;
+		extern DWORD64 ptrCrosshair;
 
 		//opcodes
-		extern DWORD64 op_freeze_player;
+		extern DWORD64 opFreeze;
 
 		//player
-		extern DWORD64 off_player;
-		extern DWORD64 off_player_health;
-		extern DWORD64 off_player_healthmax;
-		extern DWORD64 off_player_armor;
-		extern DWORD64 off_player_invisibility;
-		extern DWORD64 off_player_godmode;
-		extern DWORD64 off_player_ragdoll;
-		extern DWORD64 off_player_in_vehicle;
-		extern DWORD64 off_player_speed_x;
-		extern DWORD64 off_player_speed_y;
-		extern DWORD64 off_player_speed_z;
+		extern DWORD64 Player;
+		extern DWORD64 PlayerHealth;
+		extern DWORD64 PlayerHealthMax;
+		extern DWORD64 PlayerArmor;
+		extern DWORD64 PlayerInvisibility;
+		extern DWORD64 PlayerGodmode;
+		extern DWORD64 PlayerRagdoll;
+		extern DWORD64 PlayerInVehicle;
+		extern DWORD64 PlayerSpeedX;
+		extern DWORD64 PlayerSpeedY;
+		extern DWORD64 PlayerSpeedZ;
 
 		//playerposition
-		extern DWORD64 off_player_pos;
-		extern DWORD64 off_player_pos_x;
-		extern DWORD64 off_player_pos_y;
-		extern DWORD64 off_player_pos_z;
+		extern DWORD64 Player_Pos;
+		extern DWORD64 Player_PosX;
+		extern DWORD64 Player_PosY;
+		extern DWORD64 Player_PosZ;
 
 		//playerMisc
-		extern DWORD64 off_player_info;
-		extern DWORD64 off_player_info_nearestvehicle;
-		extern DWORD64 off_player_info_wanted_level;
-		extern DWORD64 off_player_info_sprintmp;
-		extern DWORD64 off_player_info_swimmp;
-		extern DWORD64 off_player_info_stamina;
+		extern DWORD64 Player_Info;
+		extern DWORD64 Player_InfoNearestvehicle;
+		extern DWORD64 Player_InfoWantedlvl;
+		extern DWORD64 Player_InfoSprint;
+		extern DWORD64 Player_InfoSwim;
+		extern DWORD64 Player_InfoStamina;
 
 		//vehicle 
-		extern DWORD64 off_player_vehicle;
-		extern DWORD64 off_player_vehicle_health_1;
-		extern DWORD64 off_player_vehicle_health_2;
-		extern DWORD64 off_player_vehicle_health_3;
-		extern DWORD64 off_player_vehicle_invisibility;
-		extern DWORD64 off_player_vehicle_godmode;
-		extern DWORD64 off_player_vehicle_gravity;
-		extern DWORD64 off_player_vehicle_speed_x;
-		extern DWORD64 off_player_vehicle_speed_y;
-		extern DWORD64 off_player_vehicle_speed_z;
+		extern DWORD64 Player_Vehicle;
+		extern DWORD64 Player_VehicleHealth1;
+		extern DWORD64 Player_VehicleHealth2;
+		extern DWORD64 Player_VehicleHealth3;
+		extern DWORD64 Player_VehicleInvisibility;
+		extern DWORD64 Player_VehicleRocketfuel;
+		extern DWORD64 Player_VehicleRocketspeed;
+		extern DWORD64 Player_VehicleRocketrecharge;
+		extern DWORD64 Player_VehicleDirtamount;
+		extern DWORD64 Player_VehicleBulletproof;
+		extern DWORD64 Player_VehicleGodmode;
+		extern DWORD64 Player_VehicleGravity;
+		extern DWORD64 Player_VehicleSpeedX;
+		extern DWORD64 Player_VehicleSpeedY;
+		extern DWORD64 Player_VehicleSpeedZ;
+
 
 		//vehiclehandling
-		extern DWORD64 off_player_vehicle_handling;
-		extern DWORD64 off_player_vehicle_handling_acceleration;
-		extern DWORD64 off_player_vehicle_handling_tractioncurve_min;
-		extern DWORD64 off_player_vehicle_handling_tractioncurve_max;
+		extern DWORD64 Player_Vehicle_Handling;
+		extern DWORD64 Player_Vehicle_HandlingAccelerationMP;
+		extern DWORD64 Player_Vehicle_HandlingTractionMin;
+		extern DWORD64 Player_Vehicle_HandlingTractionMax;
+		extern DWORD64 Player_Vehicle_HandlingBrakeforce;
+		extern DWORD64 Player_Vehicle_HandlingHandbrakeforce;
+		extern DWORD64 Player_Vehicle_HandlingMass;
+		extern DWORD64 Player_Vehicle_HandlingPercentSubmerged;
+		extern DWORD64 Player_Vehicle_HandlingCollissionDamage;
+		extern DWORD64 Player_Vehicle_HandlingWeaponDamage;
+		extern DWORD64 Player_Vehicle_HandlingDeformMP;
+		extern DWORD64 Player_Vehicle_HandlingThrust;
+		extern DWORD64 Player_Vehicle_HandlingThrustDeluxo;
 
 
 		//vehicleposition
-		extern DWORD64 off_player_vehicle_pos;
-		extern DWORD64 off_player_vehicle_pos_x;
-		extern DWORD64 off_player_vehicle_pos_y;
-		extern DWORD64 off_player_vehicle_pos_z;
+		extern DWORD64 Player_Vehicle_Pos;
+		extern DWORD64 Player_Vehicle_PosX;
+		extern DWORD64 Player_Vehicle_PosY;
+		extern DWORD64 Player_Vehicle_PosZ;
 
 		//weapon
-		extern DWORD64 off_weapon_current_ammo;
-		extern DWORD64 off_weapon_damage;
-		extern DWORD64 off_weapon_reload_multiplier;
+		extern DWORD64 Player_WeaponInfo_AmmoInfoAmmo;
+		extern DWORD64 Player_WeaponInfo_AmmoInfoRocketspeed;
+		extern DWORD64 Player_WeaponInfo_AmmoInfoRocketlifetime;
+
+		extern DWORD64 Player_WeaponInfoDamage;
+		extern DWORD64 Player_WeaponInfoReload;
+		extern DWORD64 Player_WeaponInfoMass;
+		extern DWORD64 Player_WeaponInfoMuzzleVel;
+		extern DWORD64 Player_WeaponInfoRange;
+		extern DWORD64 Player_WeaponInfoRecoil;
+		extern DWORD64 Player_WeaponInfoBatchSpread;
+		extern DWORD64 Player_WeaponInfoPenetration;
+		extern DWORD64 Player_WeaponInfoForceOnPed;
+		extern DWORD64 Player_WeaponInfoForceOnVehicle;
+		extern DWORD64 Player_WeaponInfoForceOnHeli;
 	}
 
 	//namespace
 
-	namespace player
+	namespace Player
 	{
-		void     set_health(float Value);
-		void     set_armor(float Value);
-		void     set_wantedlvl(int Value);
-		void     set_sprintspeed(float Value);
-		void     set_swimspeed(float Value);
-		void     set_xyz(vector3 Values);
-		void     set_x(float Value);
-		void     set_y(float Value);
-		void     set_z(float Value);
-		void     set_godmode(bool Value);
-		void     set_invisibility(bool Value);
-		void     set_ragdoll(bool Value);
-		void     set_freeze(bool Value);
+		float    Health();
+		void     Health(float Value);
+		float    Healthmax();
+		void     Healthmax(float Value);
+		float    Armor();
+		void     Armor(float Value);
+		int      Wantedlvl();
+		void     Wantedlvl(int Value);
+		float    SprintMP();
+		void     SprintMP(float Value);
+		float    SwimMP();
+		void     SwimMP(float Value);
+		vector3  XYZ();
+		void     XYZ(vector3 Values);
+		float    X();
+		void     X(float Value);
+		float    Y();
+		void     Y(float Value);
+		float    Z();
+		void     Z(float Value);
+		bool     Godmode();
+		void     Godmode(bool Value);
+		bool     Invisibility();
+		void     Invisibility(bool Value);
+		bool     Ragdoll();
+		void     Ragdoll(bool Value);
+		bool     Freeze();
+		void     Freeze(bool Value);
 
 
-		float    get_health();
-		float    get_healthmax();
-		float    get_armor();
-		float    get_sprintspeed();
-		float    get_swimspeed();
-		vector3  get_xyz();
-		float    get_x();
-		float    get_y();
-		float    get_z();
-		vector3  get_speed_xyz();
-		float    get_speed_x();
-		float    get_speed_y();
-		float    get_speed_z();
-		float    get_waypoint_x();
-		float    get_waypoint_y();
-		int      get_wantedlevel();
-		int      get_crosshair();
-		bool     get_freeze();
-		bool     get_invehicle();
-		bool     get_godmode();
-		bool     get_invisibility();
-		bool     get_ragdoll();
+		vector3  SpeedXYZ();
+		float    SpeedX();
+		float    SpeedY();
+		float    SpeedZ();
+		float    WaypointX();
+		float    WaypointY();
+		int      Crosshair();
+		bool     Invehicle();
 	}
 
-	namespace vehicle
+	namespace Vehicle
 	{
-		void     update();
+		void     Update();
 
-		void     set_xyz(vector3 Values);
-		void     set_x(float Value);
-		void     set_y(float Value);
-		void     set_z(float Value);
-		void     set_godmode(bool Value);
-		void     set_gravity(float Value);
-		void     set_acceleration(float Value);
-		void     set_traction_min(float Value);
-		void     set_traction_max(float Value);
-		void     set_health1(float Value);
-		void     set_health2(float Value);
-		void     set_health3(float Value);
+		vector3  XYZ();
+		void     XYZ(vector3 Values);
+		float    X();
+		void     X(float Value);
+		float    Y();
+		void     Y(float Value);
+		float    Z();
+		void     Z(float Value);
+		bool     Godmode();
+		void     Godmode(bool Value);
+		float    Gravity();
+		void     Gravity(float Value);
+		float    Acceleration();
+		void     Acceleration(float Value);
+		float    TractionMin();
+		void     TractionMin(float Value);
+		float    TractionMax();
+		void     TractionMax(float Value);
+		float    Health();
+		void     Health(float Value);
+		float    Health2();
+		void     Health2(float Value);
+		float    Health3();
+		void     Health3(float Value);
+		float	 RocketFuel();
+		void	 RocketFuel(float Value);
+		float	 RocketSpeed();
+		void	 RocketSpeed(float Value);
+		float	 RocketRecharge();
+		void	 RocketRecharge(float Value);
+		float    DirtAmount();
+		void     DirtAmount(float Value);
+		float    BrakeForce();
+		void     BrakeForce(float Value);
+		float	 HandbrakeForce();
+		void	 HandbrakeForce(float Value);
+		float	 Mass();
+		void	 Mass(float Value);
+		float	 CollisionDamage();
+		void	 CollisionDamage(float Value);
+		float	 WeaponDamage();
+		void	 WeaponDamage(float Value);
+		float	 Deformmp();
+		void	 Deformmp(float Value);
+		float	 Thrust();
+		void	 Thrust(float Value);
+		float	 ThrustDeluxo();
+		void	 ThrustDeluxo(float Value);
 
-		vector3  get_xyz();
-		float    get_x();
-		float    get_y();
-		float    get_z();
-		vector3  get_speed_xyz();
-		float    get_speed_x();
-		float    get_speed_y();
-		float    get_speed_z();
-		bool     get_godmode();
-		float    get_acceleration();
-		float    get_traction_min();
-		float    get_traction_max();
-		float    get_health1();
-		float    get_health2();
-		float    get_health3();
-		float    get_gravity();
+
+		vector3  SpeedXYZ();
+		float    SpeedX();
+		float    SpeedY();
+		float    SpeedZ();
 	}
 
-	namespace vehiclenearest
+	namespace Vehiclenearest
 	{
-		void     update();
+		void     Update();
 
-		void     set_xyz(vector3 Values);
-		void     set_x(float Value);
-		void     set_y(float Value);
-		void     set_z(float Value);
-		void     set_godmode(bool Value);
-		void     set_gravity(float Value);
-		void     set_acceleration(float Value);
-		void     set_traction_min(float Value);
-		void     set_traction_max(float Value);
-		void     set_health1(float Value);
-		void     set_health2(float Value);
-		void     set_health3(float Value);
+		vector3  XYZ();
+		void     XYZ(vector3 Values);
+		float    X();
+		void     X(float Value);
+		float    Y();
+		void     Y(float Value);
+		float    Z();
+		void     Z(float Value);
+		bool     Godmode();
+		void     Godmode(bool Value);
+		float    Gravity();
+		void     Gravity(float Value);
+		float    Acceleration();
+		void     Acceleration(float Value);
+		float    TractionMin();
+		void     TractionMin(float Value);
+		float    TractionMax();
+		void     TractionMax(float Value);
+		float    Health();
+		void     Health(float Value);
+		float    Health2();
+		void     Health2(float Value);
+		float    Health3();
+		void     Health3(float Value);
 
-		vector3  get_xyz();
-		float    get_x();
-		float    get_y();
-		float    get_z();
-		vector3  get_speed_xyz();
-		float    get_speed_x();
-		float    get_speed_y();
-		float    get_speed_z();
-		bool     get_godmode();
-		float    get_acceleration();
-		float    get_traction_min();
-		float    get_traction_max();
-		float    get_health1();
-		float    get_health2();
-		float    get_health3();
-		float    get_gravity();
+		vector3  SpeedXYZ();
+		float    SpeedX();
+		float    SpeedY();
+		float    SpeedZ();
 	}
 
-	namespace weapon
+	namespace Weapon
 	{
-		void     update();
+		void     Update();
 
-		void     set_damage(float Value);
-		void     set_currentammo(int Value);
-		void     set_reloadspeed(float Value);
-
-		float    get_damage();
-		float    get_reloadspeed();
-		int      get_currentammo();
+		float    Damage();
+		void     Damage(float Value);
+		int      Ammo();
+		void     Ammo(int Value);
+		float    Reload();
+		void     Reload(float Value);
+		float	 Mass();
+		void	 Mass(float Value);
+		float	 Muzzlevel();
+		void	 Muzzlevel(float Value);
+		float	 Recoil();
+		void	 Recoil(float Value);
+		float	 Range();
+		void	 Range(float Value);
+		float	 BatchSpread();
+		void	 BatchSpread(float Value);
+		float	 Penetration();
+		void	 Penetration(float Value);
+		float	 ForcOnPed();
+		void	 ForcOnPed(float Value);
+		float	 ForceOnVehicle();
+		void	 ForceOnVehicle(float Value);
+		float	 ForceOnHeli();
+		void	 ForceOnHeli(float Value);
+		float	 RocketLifetime();
+		void	 RocketLifetime(float Value);
+		float	 RocketSpeed();
+		void	 RocketSpeed(float Value);
 	}
 }
