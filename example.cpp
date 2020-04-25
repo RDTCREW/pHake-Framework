da8479493bf7479b7327e173a8aa7e55652c6d86
Dj Stalin <52607377+xhz8s@users.noreply.github.com>
2020-04-25 23:32:14 +0200

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -10,7 +10,7 @@ int main()
 		std::cout << "Failed to handle Game";
 
 	World world = World(mem.handle);
-	world.updateSub(mem.read<uint64_t>(mem.base + 0x024B0C50));
+	world.updateSub(mem.read<uint64_t>(mem.base + 0x024B0C50)); // Steam World Pointer
 
 	world.localPlayer.god(true);
 	world.localPlayer.position.xyz(vec3(1000, 1000, 50));
